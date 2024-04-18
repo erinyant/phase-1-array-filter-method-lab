3e304812eb309c8bdf6809f88ce6416386d45b50
Erin Yant <ghosrider383@gmail.com>
2024-04-18 10:32:33 -0600

    completed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, query) {
+    if (drivers.length === 0) {
+      return [] 
+    // return empty if driver array empty
+    }
+  
+    const matches = []
+  
+    for (const driver of drivers) {
+      if (driver.startsWith(query)) {
+        matches.push(driver);
+      }
+    }
+    return matches
+  }
+
+function matchName(drivers, name) {
+    const matchingDrivers = []
+// create empty array to store matching driver objects
+    drivers.forEach(driver => {
+        if (driver.name === name) {
+            matchingDrivers.push(driver)
+        }
+// for loop - iterate over driver objects in array
+// check if name property matches provided string argument - if so, push to empty array
+    })
+    return matchingDrivers
+}
+
